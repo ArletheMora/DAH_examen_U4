b102447fde6db5ac3f8c1cb8dc00d90091b0fc76
ArletheMora <armomorali@ittepic.edu.mx>
2022-12-12 18:07:42 -0700

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD3WvP-1YyIGHpRaqAjq2yOioQ7cHpt1s4",
+    authDomain: "dah-u4examen.firebaseapp.com",
+    projectId: "dah-u4examen",
+    storageBucket: "dah-u4examen.appspot.com",
+    messagingSenderId: "607622083737",
+    appId: "1:607622083737:web:b8703829e5481a4c97b408"
+  }
 };
